06570fc0c5639f4d9061947c60e90ebec2976c48
Veltinho20 <santoserivelton021@gmail.com>
2026-06-05 22:48:39 -0300

    Implementada a classe projetoRepository

---
+++ src/main/java/br/ufc/poo/repository/UsuarioRepository.java
@@ -0,0 +1,31 @@
+package br.ufc.poo.repository;
+
+import java.util.ArrayList;
+import br.ufc.poo.model.Usuario;
+
+public class UsuarioRepository {
+
+    private ArrayList<Usuario> usuarios;
+
+    public UsuarioRepository() {
+        this.usuarios = new ArrayList<>();
+    }
+
+    public void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public ArrayList<Usuario> listarUsuarios() {
+        return usuarios;
+    }
+
+    public Usuario buscarPorEmail(String email) {
+
+        for (Usuario u : usuarios) {
+            if (u.getEmail().equals(email)) {
+                return u;
+            }
+        }
+        return null;
+    }
+}
